ec7af647f543d07a2f529058e071977d2090b044
Agustin Fiori <afiori@clarika.com.ar>
2020-05-18 16:04:27 -0300

    Model working

--- src/app.js
+++ src/app.js
@@ -1,6 +1,30 @@
 const express = require('express');
+const path = require('path');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
 const app = express();
 
-app.listen(3000, () => {
-    console.log('Server on port 3000');
+// DB
+mongoose.connect('mongodb://localhost/node-todo')
+    .then(db => console.log('DB Connected!'))
+    .catch(err => console.log(err));
+
+// IMPORT ROUTES
+const indexRoutes = require('./routes/index');
+
+// SETTINGS
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// MIDDLEWARE
+app.use(morgan('dev'));
+app.use(express.urlencoded( { extended: false } ));
+
+// ROUTES
+app.use('/', indexRoutes);
+
+// START SERVER
+app.listen(app.get('port'), () => {
+    console.log(`Server on port ${app.get('port')}`);
 });
